3413782d932ed0f8f552077c60dd58f8e229a6a1
xTeKc <bcd3v@pm.me>
2021-06-01 18:00:59 -0400

    update

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,7 @@ main = someFunc
 parse :: 
 parse = strOption
 	where
-		strOption :: IsString s
+		strOption :: IsString s =>
 		strOption =
 
 getArgs :: [String] -> [String] -> [String] -> [String] -> [String]
