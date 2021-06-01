9e948a310105b366024fda43d5f59fc6c2ac509c
xTeKc <bcd3v@pm.me>
2021-06-01 18:09:03 -0400

    update

--- app/Main.hs
+++ app/Main.hs
@@ -12,9 +12,7 @@ parse ::
 parse = strOption
 	where
 		strOption :: IsString s => Mod OptionFields s -> Parser s
-		strOption =
-
-getArgs :: [String] -> [String] -> [String] -> [String] -> [String]
+		strOption = 
 
 
 
