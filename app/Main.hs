6721d45af9f074a1c3c6a56569ac93070e9dd96f
xTeKc <bcd3v@pm.me>
2021-06-07 19:43:44 -0400

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,7 @@
 module Main where
 
 import Lib
-import System.Process
-import Control.Applicative
-import Options.Applicative
-import Data.Semigroup hiding (option)
-import Data.String (fromString, IsString)
 
 main :: IO ()
 main = someFunc
 
-
---parse :: [String] -> [String]
-parse = strOption
-	where
-		strOption :: IsString s => Mod OptionFields s -> Parser s
-		strOption = "-a" "--adapter" 
-
-
-
-changeMac :: [String] -> [String]
-
-
-
-getCurrentMac ::
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,26 @@ module Lib
     ( someFunc
     ) where
 
+import System.Process
+import Control.Applicative
+import Options.Applicative
+import Data.Semigroup hiding (option)
+import Data.String (fromString, IsString)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+--parse :: [String] -> [String]
+parse = strOption
+    where
+        strOption :: IsString s => Mod OptionFields s -> Parser s
+        strOption = "-a" "--adapter" 
+
+
+
+changeMac :: [String] -> [String]
+
+
+
+getCurrentMac ::
