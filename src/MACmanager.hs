d22eaf461f1cc0f015d10ea483282a960253a525
xTeKc <bcd3v@pm.me>
2021-06-11 22:51:00 -0400

    update

--- src/MACmanager.hs
+++ src/MACmanager.hs
@@ -11,10 +11,7 @@ manageMAC = putStrLn "manageMAC"
 
 
 getCurrentMac :: String -> RoutePattern
-getCurrentMac = get (getCurrentMac "^f(.*)r$") $ do
-    path <- param "0"
-    cap <- param "1"
-    text $ mconcat ["Path: ", path, "\nCapture: ", cap]
+getCurrentMac = 
 
 --generateNewMAC :: String ->
 generateNewMAC = "_ _:_ _:_ _:_ _:_ _:_ _"
